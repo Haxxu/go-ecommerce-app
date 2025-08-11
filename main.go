1ce7dfea9bb6b50fd6af95d24b15a6a7f4e19f59
Minh <nguyenngocminh190901@gmail.com>
2025-08-11 22:17:21 +0700

    fdf

--- main.go
+++ main.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"fmt"
 	_ "go-ecommerce-app/config"
 
 	"github.com/gofiber/fiber/v2"
@@ -10,22 +9,20 @@ import (
 func main() {
 	app := fiber.New()
 
-	//var myFamily [3]string
-	//myFamily[0] = "A"
-	//myFamily[1] = "B"
-	//myFamily[2] = "C"
+	//myWishlist := make(map[string]string)
+	//myWishlist["first"] = "Health"
+	//myWishlist["second"] = "100 Million Dollar"
+	//myWishlist["third"] = "Beautiful Wife"
+	//myWishlist["fourth"] = "Beautiful Wife"
+	//
+	//delete(myWishlist, "fourth")
+	//
+	//fmt.Printf("My wishlist is %v\n", myWishlist)
 
-	//myFamily := [3]string{"A", "B", "C"}
-
-	var myFriends []string
-	myFriends = append(myFriends, "John", "Jane")
-
-	myCources := [][]string{
-		{"Go", "NodeJS"},
-		{"AWS", "GCP"},
+	type Product struct {
+		Name  string
+		Price float64
 	}
-
-	fmt.Println(myCources)
 
 	app.Listen(":9000")
 }
